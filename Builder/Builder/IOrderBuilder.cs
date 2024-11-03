75212117689bd6490c06724ea6b25d84d9cbf6cd
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-11-03 13:04:10 -0300

    feat: add order builder

---
+++ Builder/Builder/IOrderBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Builder.Models;
+
+namespace Builder.Builder;
+
+public interface IOrderBuilder
+{
+    IOrderBuilder SetCustomer(string name);
+    IOrderBuilder SetShippingAddress(string address);
+    IOrderBuilder SetAmount(decimal amount);
+    IOrderBuilder SetPaymentMethod(PaymentMethod method);
+    IOrderBuilder AddItems(List<string> items);
+    Order Build();
+}
