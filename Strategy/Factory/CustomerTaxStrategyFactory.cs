0ff5ac3691c17e1f3316b3d8198c0ab50108a767
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-11-03 21:42:08 -0300

    feat: add strategy factory method

---
+++ Strategy/Factory/CustomerTaxStrategyFactory.cs
@@ -0,0 +1,24 @@
+﻿using Strategy.Models;
+using Strategy.Strategy;
+
+namespace Strategy.Factory;
+
+public class CustomerTaxStrategyFactory : ICustomerTaxStrategyFactory
+{
+    private readonly IDictionary<CustomerType, ICustomerTaxStrategy> _strategies;
+
+    public CustomerTaxStrategyFactory(IDictionary<CustomerType, ICustomerTaxStrategy> strategies)
+    {
+        _strategies = strategies;
+    }
+
+    public ICustomerTaxStrategy GetStrategy(CustomerType customerType)
+    {
+        if(_strategies.TryGetValue(customerType, out var strategy))
+        {
+            return strategy;
+        }
+
+        throw new NotSupportedException($"Customer type '{customerType}' is not supported.");
+    }
+}
