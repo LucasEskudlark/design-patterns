adfa8c418e1da97097c42151afb3a7203d270294
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-11-03 21:42:42 -0300

    chore: add dependency injection and service registration

---
+++ Strategy/Extensions/StrategyConfigurationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Strategy.Factory;
+using Strategy.Models;
+using Strategy.Strategy;
+
+namespace Strategy.Extensions;
+
+public static class StrategyConfigurationExtensions
+{
+    public static void ConfigureStrategy(this IServiceCollection services)
+    {
+        var strategyType = typeof(ICustomerTaxStrategy);
+        var strategyTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => strategyType.IsAssignableFrom(p) && !p.IsInterface);
+
+        foreach (var type in strategyTypes)
+        {
+            services.AddSingleton(type);
+            services.AddSingleton(strategyType, serviceProvider =>
+                serviceProvider.GetRequiredService(type));
+        }
+
+        services.AddSingleton<ICustomerTaxStrategyFactory>(serviceProvider =>
+        {
+            var strategies = serviceProvider.GetServices<ICustomerTaxStrategy>();
+            var strategyDictionary = strategies.ToDictionary(
+                strategy => strategy.CustomerType,
+                strategy => strategy);
+
+            return new CustomerTaxStrategyFactory(strategyDictionary);
+        });
+    }
+}
