f806bc3e2d7be23fb6b7b0d1fbb8e415e642580d
LucasEskudlark <lucasbeskudlark@gmail.com>
2024-11-11 21:10:00 -0300

    feature: add data importers and config

---
+++ TemplateMethod/Extensions/TemplateMethodConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TemplateMethod.Template;
+
+namespace TemplateMethod.Extensions;
+
+public static class TemplateMethodConfigurationExtensions
+{
+    public static void ConfigureTemplateMethod(this IServiceCollection services)
+    {
+        services.AddScoped<IDataImporter, SqlDataImporter>();
+    }
+}
---
+++ TemplateMethod/Template/DataImporter.cs
@@ -0,0 +1,22 @@
+﻿namespace TemplateMethod.Template;
+
+public abstract class DataImporter : IDataImporter
+{
+    public void ImportData()
+    {
+        Connect();
+        ExtractData();
+        TransformData();
+        LoadData();
+        Disconnect();
+    }
+    
+    public abstract void Connect();
+    public abstract void ExtractData();
+    public virtual void TransformData()
+    {
+        Console.WriteLine("Transforming data in abstract class");
+    }
+    public abstract void LoadData();
+    public abstract void Disconnect();
+}
